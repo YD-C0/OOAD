a5c3c3d20cdbc54276d0251aa8bba7f3b4169c60
YD-C0 <yadabcode@gmail.com>
2025-02-26 19:14:57 +0700

    1.6

--- executive/history_fixed/connect.php
+++ executive/history_fixed/connect.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "ooad";
+    $dbname = "intpro";
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
